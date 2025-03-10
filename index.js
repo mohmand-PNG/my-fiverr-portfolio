07e2d39ef36bd627dde71d4977a2779df4082ff4
Marwa <marwamohmand13@gmail.com>
2025-03-10 10:39:00 +0430

    project setup

---
+++ index.js
@@ -0,0 +1,64 @@
+var app = document.querySelector(".typing");
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(2500)
+  .typeString("Developer")
+  .pauseFor(300)
+  .deleteChars(10)
+  .pauseFor(1000)
+  .start();
+
+// document.addEventListener("DOMContentLoaded", type);
+
+document.addEventListener("DOMContentLoaded", function () {
+  const serviceCards = document.querySelectorAll(".services .service-card");
+
+  function revealCards() {
+    serviceCards.forEach((card) => {
+      const cardTop = card.getBoundingClientRect().top;
+
+      const windowHeight = window.innerHeight;
+
+      if (cardTop < windowHeight - 50) {
+        card.classList.add("show");
+      }
+    });
+  }
+
+  window.addEventListener("scroll", revealCards);
+
+  revealCards(); // Trigger animation on page load if cards are already in view
+});
+
+// contact section
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the heading and paragraph in the Contact section
+  const heading = document.querySelector("#contact h2");
+  const paragraph = document.querySelector("#contact p");
+
+  function revealElements() {
+    // Get the position of the heading and paragraph relative to the viewport
+    const headingTop = heading.getBoundingClientRect().top;
+    const paragraphTop = paragraph.getBoundingClientRect().top;
+
+    const windowHeight = window.innerHeight;
+
+    // Reveal elements if they are within the viewport
+    if (headingTop < windowHeight - 50) {
+      heading.classList.add("show");
+    }
+    if (paragraphTop < windowHeight - 50) {
+      paragraph.classList.add("show");
+    }
+  }
+
+  window.addEventListener("scroll", revealElements);
+
+  revealElements(); // Trigger animation on page load if elements are already in view
+});
